f6da9178f551daa496e74140ff5313f501e35250
Kaleb <kaleb.goering@gmail.com>
2020-02-11 17:22:59 -0700

    Added load_mysql()

--- src/brain.py
+++ src/brain.py
@@ -5,3 +5,12 @@ def load_config(filename):
     filestream = open(filename, 'r')
     yaml_data = yaml.load(filestream)
     return yaml_data
+
+
+def load_mysql(db_login):
+    conn = mysql.connect(
+        host=db_login['dbhost'],
+        user=db_login['user'],
+        passwd=db_login['pass'],
+        database=db_login['dtbs'])
+    return conn
