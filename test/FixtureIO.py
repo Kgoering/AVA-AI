ba55b8e7fb895fd4ff9624f2b0a4b426b68e218b
Kaleb <kaleb.goering@gmail.com>
2020-02-15 09:11:39 -0700

    Fixed unit tests

--- test/FixtureIO.py
+++ test/FixtureIO.py
@@ -1,7 +1,8 @@
 import unittest
 import io
-from mock import patch
+from unittest.mock import patch
 import sys
+import builtins
 
 sys.path.append('../src/skills')
 
@@ -10,15 +11,16 @@ import AVA_IO
 
 class TestAVAIO(unittest.TestCase):
     def test_text_read(self):
-        with patch('__builtin__.raw_input', return_value='Hello World') as text:
+        with patch('builtins.input', return_value='Hello World') as text:
             io_obj = AVA_IO.TextIO()
             input = io_obj.read()
-            self.assetEqual("Hello World", input)
+            self.assertEqual("hello world", input)
 
     def test_text_write(self):
         io_obj = AVA_IO.TextIO()
+        io_obj.set_ai_name("AVA")
         capturedOutput = io.StringIO()
         sys.stdout = capturedOutput
         io_obj.write('Hello World')
         sys.stdout = sys.__stdout__
-        self.assetEqual('Hello World', capturedOutput.getvalue())
+        self.assertEqual('AVA> Hello World\n', capturedOutput.getvalue())
